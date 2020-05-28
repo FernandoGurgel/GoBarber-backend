5fa9736e8198356333ff04916c9daae0faa748ab
Fernando Gurgel <fag.gurgel@gmail.com>
2020-05-27 21:37:22 -0400

    Validated authentication

---
+++ src/routes/Sessions.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+import AuthenticateUserService from '../services/AuthenticateUserService'
+
+const SessionsRoutes = Router()
+
+SessionsRoutes.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body
+    const authenticateUserService = new AuthenticateUserService()
+    const { user } = await authenticateUserService.execute({ email, password })
+    return response.json({ user })
+  } catch (err) {
+    return response.status(400).json({ error: err.message })
+  }
+})
+
+export default SessionsRoutes
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import AppointmentsRoute from './Appointments.routes'
 import UsersRoute from './Users.routes'
+import SessionsRoute from './Sessions.routes'
 
 const routes = Router()
 
 routes.use('/appointments', AppointmentsRoute)
 routes.use('/users', UsersRoute)
+routes.use('/sessions', SessionsRoute)
 
 export default routes
